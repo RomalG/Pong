91e7f0803a2b40a6c2038ca7e9bf73f5fb7f9305
Romal George <rgeorg32@my.centennialcollege.ca>
2020-01-29 16:59:28 -0500

    Added ball Physics

---
+++ Assets/Bat1Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat1Controller : MonoBehaviour
+{
+    private Rigidbody2D p1;
+    Vector2 v1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p1 = this.GetComponent<Rigidbody2D>();
+        //p1.position = new Vector2(-14.09f, 0.03550553f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            p1.velocity = new Vector2(0f, 17f); ;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            p1.velocity = new Vector2(0f, -17f);
+        }
+        else
+        {
+            p1.velocity = new Vector2(0f, 0f); ;
+        }
+    }
+}
---
+++ Assets/Bat2Controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat2Controller : MonoBehaviour
+{
+    private Rigidbody2D p2;
+    Vector2 v2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p2 = GetComponent<Rigidbody2D>();
+        //p1.position = new Vector2(-14.09f, 0.03550553f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+
+            //Debug.Log(v1.x);
+            //Debug.Log(v1.y);
+            p2.velocity = new Vector2(0f, 17f); ;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            p2.velocity = new Vector2(0f, -17f);
+        }
+        else
+        {
+            p2.velocity = new Vector2(0f, 0f); ;
+        }
+    }
+}
